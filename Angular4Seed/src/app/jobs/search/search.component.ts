26b2ecfcf6ac8e9081ed3429641c01e80362622b
Hassan Humayun <hassan.humayun@gmail.com>
2017-11-09 16:50:37 +0800

    Check in - Alert functionality v1

--- Angular4Seed/src/app/jobs/search/search.component.ts
+++ Angular4Seed/src/app/jobs/search/search.component.ts
@@ -1,45 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { JobService } from '../jobs.service';
 import { Job } from '../job.models';
 import { LoadingService } from '../../shared/loading/loading.service';
+import { AlertService } from '../../shared/alert/alert.service';
+import { Alert, AlertType } from '../../shared/alert/alert';
 
 @Component({
-    selector: 'app-search',
-    templateUrl: './search.component.html',
-    styleUrls: ['./search.component.css'],
-    providers: [JobService]
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css'],
+  providers: [JobService]
 })
-export class SearchComponent implements OnInit {
-    searchKey: string;
-    public jobs: Array<Job>;
-    constructor(
-        private route: ActivatedRoute,
-        private router: Router,
-        private jobService: JobService,
-        private loadingService: LoadingService
-    ) {
-        this.jobs = new Array<Job>();
-    }
+export class SearchComponent implements AfterContentInit {
+  searchKey: string;
+  public jobs: Array<Job>;
+  constructor( 
+    private route: ActivatedRoute,
+    private router: Router,
+    private jobService: JobService,
+    private loadingService: LoadingService,
+    private alertService: AlertService
+  ) {
+    this.jobs = new Array<Job>();
+  }
 
-    ngOnInit() {
-        this.loadingService.show();
-        let search: string = this.route.snapshot.params['id'];
-        if (search && search.length > 0) {
-            this.jobService.search(search).subscribe((result: Array<Job>) => {
-                this.jobs = result;
-                this.loadingService.hide();
-            });
-        } else {
-            this.jobService.search("").subscribe((result: Array<Job>) => {
-                this.jobs = result;
-                this.loadingService.hide();
-            });
-        }
-    }
+  ngAfterContentInit() {
+    this.loadingService.show();
+    let search: string = this.route.snapshot.params['id'];
+    this.DoSearch(search);
+  }
 
-    OnSerach() {
-        console.log(this.searchKey);
-        this.router.navigate(['/jobs/search', this.searchKey]);
-    }
+  DoSearch(s: string) {
+    this.jobService.search(s).subscribe((result: Array<Job>) => {
+      this.jobs = result;
+
+      if (this.jobs.length == 0) {
+        this.alertService.error('No results found!', false);
+      }
+      
+      if (this.searchKey == undefined) {
+        //this.alertService.info(this.jobs.length + ' jobs found!', false);
+        this.alertService.info('You can now search job!', true);
+      }
+      else if (this.searchKey != undefined && this.jobs.length > 0) {
+        this.alertService.success(this.jobs.length + ' jobs found!', false);
+      }
+      
+      this.loadingService.hide();
+    });
+  }
+
+  OnSerach() {
+    this.alertService.hide();
+    this.DoSearch(this.searchKey);
+
+  }
 }
--- Angular4Seed/src/app/shared/shared.module.ts
+++ Angular4Seed/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingComponent } from './loading/loading.component';
 import { LoadingService } from './loading/loading.service';
+import { AlertService } from './alert/alert.service';
 
 @NgModule({
     imports: [
@@ -14,7 +15,7 @@ export class SharedModule {
     static forRoot(): ModuleWithProviders {
         return {
             ngModule: SharedModule,
-            providers: [LoadingService]
+            providers: [LoadingService, AlertService]
         };
     }
 
